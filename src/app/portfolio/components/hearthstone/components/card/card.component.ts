d116fa12c4dac56ca2feb46af384253e442ab5aa
Sam Maclean <s.maclean1988@gmail.com>
2018-05-28 14:18:41 +0100

    made hearthstone component response

--- src/app/portfolio/components/hearthstone/components/card/card.component.ts
+++ src/app/portfolio/components/hearthstone/components/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -6,6 +6,21 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./card.component.css']
 })
 export class CardComponent implements OnInit {
+
+  // viewMode: string;
+
+  // @HostListener('window:resize', ['$event'])
+  // onResize(event) {
+  //   console.log('window resize:::', event)
+  //   if(event.target.innerWidth >= 900)
+  //     this.viewMode = 'full-size';
+
+  //   if(event.target.innerWidth <= 900 && event.target.innerWidth > 640)
+  //     this.viewMode = 'reduced';
+
+  //   if(event.target.innerWidth <= 640)
+  //     this.viewMode = 'compact';
+  // }
 
   @Input() card: any;
   @Input() set: string;
